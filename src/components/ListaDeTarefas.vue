38b1bf358cb088554e82e4ef93b6ab33fabed6bd
Isabela Ramos <isamrteixeira@gmail.com>
2024-02-19 22:43:09 -0300

    criando componentes

---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,11 @@
+<template>
+  <ul class="list-group mt-4">
+    <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
+      <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+        :id="tarefa.titulo" type="checkbox">
+      <label class="ms-3" :class="{ done: tarefa.finalizada }" :for="tarefa.titulo">
+        {{ tarefa.titulo }}
+      </label>
+    </li>
+  </ul>
+</template>
